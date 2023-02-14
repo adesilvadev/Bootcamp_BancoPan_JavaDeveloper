d4c0fbf4421f7344b8f50fe4f06dee03b6eebd06
adesilvadev <adesilva.dev@gmail.com>
2023-02-14 04:21:59 -0300

    Conceito de métodos.

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,16 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Olá turma, sejam bem vindos!");
+
+        String primeiroNome = "Ademilton";
+        String segundoNome = "Silva";
+
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+        return "Resultado do metodo:" + " " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
