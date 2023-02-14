b800599a0245f2489d0f7cabf94d4e2a19087ce4
adesilvadev <adesilva.dev@gmail.com>
2023-02-14 04:37:08 -0300

    Conceitos de identação.

---
+++ src/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+
+        int mediaFinal = 6;
+
+        if (mediaFinal > 6)
+            System.out.println("APROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA DE RECUPERAÇÃO");
+        else
+            System.out.println("REPROVADO");
+
+        }
+    }
